ff42345aca6faa57e5d62aa82b313097ae74daa4
vedi <shubin.fedor@gmail.com>
2015-08-26 11:45:58 +0300

    initial commit

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	public Text ResultText;
+
+	// Use this for initialization
+	void Start () {
+//		ProfileEvents.OnSoomlaProfileInitialized += delegate() {
+//			Debug.Log("OnSoomlaProfileInitialized");
+//		};
+//		SoomlaProfile.Initialize();
+	}
+	
+	public void OnSync() {
+        ResultText.text = "OnSync";
+	}
+
+	public void OnLeaderboards() {
+        ResultText.text = "OnLeaderboards";
+    }
+
+	public void OnGifting() {
+        ResultText.text = "OnGifting";
+    }
+
+	public void OnInsights() {
+        ResultText.text = "OnInsights";
+    }
+}
